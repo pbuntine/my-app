795c390857cbc6eaffac6612c905708225a8da3e
Philip Buntine <pbuntine@softwaredevelopment.today>
2025-12-19 15:36:25 +0000

    Adding export parameters to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  // Optional: recommended for S3 hosting to avoid broken links
+  trailingSlash: true, 
+  images: {
+    unoptimized: true, // S3 cannot optimize images on the fly like Vercel
+  },
 };
 
 export default nextConfig;
